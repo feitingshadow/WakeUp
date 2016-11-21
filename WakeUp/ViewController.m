afa3474c23c090da467892fd6afebcaf7557a33c
Stephen Johnson <stephen.johnson@synapse.com>
2016-11-21 09:48:48 -0700

    Initial Commit

---
+++ WakeUp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WakeUp
+//
+//  Created by Stephen Johnson on 11/21/16.
+//  Copyright © 2016 Nazca. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
